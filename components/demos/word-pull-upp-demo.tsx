f7452e94fc2c61411dc464e90e442354a64b584c
modouaicha023 <modouaichadiop@gmail.com>
2024-10-03 00:01:09 +0000

    add service sections

---
+++ components/demos/word-pull-upp-demo.tsx
@@ -0,0 +1,10 @@
+import WordPullUp from "@/components/ui/word-pull-up";
+
+export function WordPullUpDemo() {
+  return (
+    <WordPullUp
+      className="text-3xl md:text-5xl md:text-center font-medium"
+      words="Construit à partir de zéro"
+    />
+  );
+}
